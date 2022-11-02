1e9dd90a2e279ddc8836c1d034cc49bbd1b70a49
your_username <dannyclassic56@gmail.com>
2022-11-02 16:40:41 +0100

    added few changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const intro = document.querySelector(".introContent");
+const outro = document.querySelector(".community");
 
 gsap.to(intro, {
   backgroundColor: "rgb(53, 53, 53)",
@@ -15,8 +16,35 @@ gsap.to(intro, {
   },
 });
 
+gsap.to(outro, {
+  backgroundColor: "rgb(53, 53, 53)",
+  color: "white",
+
+  scrollTrigger: {
+    trigger: outro,
+    toggleActions: "restart pause reverse continue",
+    start: "top 20%",
+
+    scrub: 1,
+  },
+});
+
 //hamburger Menu functionality
 const burger = document.querySelector(".hamBurger");
+const burger2 = document.querySelector(".hamBurger2");
+const burger3 = document.querySelector(".hamBurger3");
+const socialTab = document.querySelector(".socials");
+
 burger.addEventListener("click", function () {
   burger.classList.toggle("change");
+});
+
+burger2.addEventListener("click", function () {
+  socialTab.classList.add("drop");
+  burger3.classList.add("change");
+});
+
+burger3.addEventListener("click", function () {
+  socialTab.classList.remove("drop");
+  burger3.classList.remove("change");
 });
