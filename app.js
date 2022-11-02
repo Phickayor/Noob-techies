48fd61131b36065bb1ec2f2d417bce7547d5be19
your_username <dannyclassic56@gmail.com>
2022-11-02 11:56:22 +0100

    added GSAP

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const intro = document.querySelector(".introContent");
+
+gsap.to(intro, {
+  backgroundColor: "rgb(53, 53, 53)",
+  color: "white",
+
+  scrollTrigger: {
+    trigger: intro,
+    toggleActions: "restart pause reverse continue",
+    start: "top 40%",
+
+    scrub: 1,
+  },
+});
+
 //hamburger Menu functionality
 const burger = document.querySelector(".hamBurger");
 burger.addEventListener("click", function () {
